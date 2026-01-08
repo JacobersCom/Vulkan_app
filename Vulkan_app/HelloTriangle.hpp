9c0324495834b707fa543c7d325dc70320e5f31e
JacoberCom <jacobymartin1@icloud.com>
2026-01-07 22:12:23 -0500

    Init commit

---
+++ Vulkan_app/HelloTriangle.hpp
@@ -0,0 +1,44 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangle
+{
+public:
+
+
+	void run()
+	{
+		InitWindow();
+		InitVulkan();
+		Update();
+		CleanUp();
+	}
+
+private:
+
+	void InitWindow()
+	{
+
+	}
+
+	void InitVulkan()
+	{
+
+	}
+
+	void Update()
+	{
+
+	}
+
+	void CleanUp()
+	{
+
+	}
+
+
+};
