165d343569b7799f522b2ce256b20c9b55f813e5
JacoberCom <jacobymartin1@icloud.com>
2026-01-07 22:58:42 -0500

    Created a instance of vulkan

--- Vulkan_app/HelloTriangle.hpp
+++ Vulkan_app/HelloTriangle.hpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
 
 class HelloTriangle
 {
@@ -11,6 +12,9 @@ private:
 
 	int width = 700, height = 700;
 	GLFWwindow* glfwWindow;
+
+	//Handle to instance
+	VkInstance Instance;
 
 public:
 
@@ -38,7 +42,8 @@ private:
 
 	void InitVulkan()
 	{
-
+		//Connects the vulkan lib to my applcation
+		CreateInstance();
 	}
 
 	void Update()
@@ -56,5 +61,60 @@ private:
 		glfwTerminate();
 	}
 
+	void CreateInstance()
+	{
+		//sType can't be null
+
+		//Information about the applcation
+		VkApplicationInfo appInfo{};
+		appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+		appInfo.pApplicationName = "Vulkan app";
+		appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.pEngineName = "No engine";
+		appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
+		appInfo.apiVersion = VK_API_VERSION_1_0;
+
+		//Information about the created Instanced
+		VkInstanceCreateInfo InstanceInfo{};
+		InstanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+		InstanceInfo.pApplicationInfo = &appInfo;
+
+		uint32_t glfwExtensionsCount = 0;
+		const char** glfwExtensions;
+
+		//Passes a array of vulkan extensions needed for glfw to create vulkan surfaces
+		glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionsCount);
+
+		InstanceInfo.enabledExtensionCount = glfwExtensionsCount;
+		InstanceInfo.ppEnabledExtensionNames = glfwExtensions;
+		//Count of enabled validation layers
+		InstanceInfo.enabledLayerCount = 0;
+
+		VkResult result = vkCreateInstance(&InstanceInfo, nullptr, &Instance);
+
+		if (result != VK_SUCCESS)
+		{
+			std::runtime_error("ERROR::Failed to create VkInstance");
+		}
+
+		uint32_t ExtensionCount = 0;
+
+		//total number of avaliable extensions
+		vkEnumerateInstanceExtensionProperties(nullptr, &ExtensionCount, nullptr);
+
+		//Array of extensionproperties. The size is determined by the count
+		std::vector<VkExtensionProperties> extension(ExtensionCount);
+
+		//Returns the name and version of avaliable extension
+		vkEnumerateInstanceExtensionProperties(nullptr, &ExtensionCount, extension.data());
+
+		std::cout << "avaliable extension" << std::endl;
+
+		for (const auto& ex : extension)
+		{
+			std::cout << ex.extensionName << "avaliable" << std::endl;
+		}
+
+	}
 	
 };
